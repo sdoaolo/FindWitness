4622f75bd6c0feb2337c8e253a4b452a603fa741
sdoaolo <09jieun01@naver.com>
2020-08-13 15:56:13 +0900

    add chat listview

---
+++ app/src/main/java/com/example/findwitness/ChatListViewItem.java
@@ -0,0 +1,18 @@
+package com.example.findwitness;
+
+public class ChatListViewItem {
+    private String userName;
+
+    public ChatListViewItem(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+}
--- app/src/main/java/com/example/findwitness/MainChatFragment.java
+++ app/src/main/java/com/example/findwitness/MainChatFragment.java
@@ -1,13 +1,20 @@
 package com.example.findwitness;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 public class MainChatFragment extends Fragment {
+    ArrayList<ChatListViewItem> chatList;
     public MainChatFragment() {
         // Required empty public constructor
     }
@@ -20,11 +27,34 @@ public class MainChatFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-       /* RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
 
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        ChatRecyclerAdapter adapter = new ChatRecyclerAdapter();
-        recyclerView.setAdapter(adapter);*/
+        TextView recent_list_date = view.findViewById(R.id.recent_list_date);
+        TextView recent_list_time = view.findViewById(R.id.recent_list_time);
+
+        //please set Text View of Recent List Info (date, time)
+
+        //listview
+        this.InitializeMovieData();
+
+        ListView listView = view.findViewById(R.id.recent_list);
+        final ChatListViewAdapter myAdapter = new ChatListViewAdapter(getActivity(),chatList);
+
+        listView.setAdapter(myAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id){
+                Log.d("cccccccccccccc","item clicked: "+myAdapter.getItem(position).getUserName()); //
+
+            }
+        });
+    }
+    public void InitializeMovieData()
+    {
+        chatList = new ArrayList<ChatListViewItem>();
+
+        chatList.add(new ChatListViewItem("test 1"));
+        chatList.add(new ChatListViewItem("test 2"));
     }
 }
+
