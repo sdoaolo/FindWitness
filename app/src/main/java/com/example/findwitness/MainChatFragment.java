f27b22726cc4722c40d0d477f2a321b724b4ee38
LeeJiHey <gx530@naver.com>
2020-08-13 16:25:25 +0900

    git test

--- app/src/main/java/com/example/findwitness/MainChatFragment.java
+++ app/src/main/java/com/example/findwitness/MainChatFragment.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
 
+
 public class MainChatFragment extends Fragment {
     ArrayList<ChatListViewItem> chatList;
     public MainChatFragment() {
